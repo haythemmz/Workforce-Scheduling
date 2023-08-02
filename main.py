120fe008d7fa34b383f7fb9da9a4fdd733e5714d
haythemmz <haythemwc@gmail.com>
2023-08-02 12:50:47 -0400

    first version of all weekdays and 3 employees

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from pulp import *
 
-def get_possible_shifts(opening_hour, closing_hour, min_hours_per_shift, max_hours_per_shift):
-    possible_shifts = []
-    shift_start = opening_hour
-
-    # Loop until we reach the closing hour
-    while shift_start + min_hours_per_shift <= closing_hour:
-        shift_end = shift_start + min_hours_per_shift
-
-        # Loop through all possible shift end times within the maximum hours per shift
-        while shift_end <= closing_hour and shift_end - shift_start <= max_hours_per_shift:
-            possible_shifts.append((shift_start, shift_end))
-            shift_end += 1  # Increment the shift end time
-
-        shift_start += 1  # Increment the shift start time
-
-    return possible_shifts
-
 def weekly_possible_shifts(l):
     union_set = set()
 
@@ -27,103 +10,142 @@ def weekly_possible_shifts(l):
 
     # Convert the union_set back to a list and return
     return list(union_set)
+    
+def get_possible_shifts(opening_hour, closing_hour, min_hours_per_shift, max_hours_per_shift):
+    possible_shifts = []
+    shift_start = opening_hour
 
-num_employees = 2
-opening = {
-    "Monday": (8, 16),
-    "Tuesday": (8, 21),
-    "Wednesday": (8, 13)
-    # Add shifts for other days as needed
-}
-C = 10  # Cost per employee/hour (same for all employees/hours)
+    while shift_start + min_hours_per_shift <= closing_hour:
+        shift_end = shift_start + min_hours_per_shift
+
+        while shift_end <= closing_hour and shift_end - shift_start <= max_hours_per_shift:
+            possible_shifts.append((shift_start, shift_end))
+            shift_end += 1
+
+        shift_start += 1
+
+    return possible_shifts
+
+def check_employee_availability(opening_hours, availability):
+    all_hours = set()
+    for day, (opening_hour, closing_hour) in opening_hours.items():
+        all_hours.update(range(opening_hour, closing_hour + 1))
+
+    available_hours_per_day = {
+        day: {hour for employee in availability for hour in range(availability[employee][day][0], availability[employee][day][1] + 1)}
+        for day in opening_hours.keys()
+    }
+
+    for day, (opening_hour, closing_hour) in opening_hours.items():
+        unavailable_hours = all_hours.difference(available_hours_per_day[day])
+        unavailable_hours = [hour for hour in unavailable_hours if opening_hour <= hour < closing_hour]
+        if unavailable_hours:
+            print(f"No employee is available on {day} at hours: {sorted(unavailable_hours)}")
+        else:
+            print(f"At least one employee is available on {day} at all hours.")
+
 min_weekly_hours = 10
 max_weekly_hours = 40
+opening = {
+    "Monday": (8, 16),
+    "Tuesday": (8, 20),
+    "Wednesday": (8, 13),
+    "Thursday": (8, 17),
+    "Friday": (9, 18),
+    "Saturday": (10, 15),
+    "Sunday": (11, 14)
+}
 
 availability = {
     'Employee1': {
         'Monday': (8, 20),
-        'Tuesday': (8, 14),
-        'Wednesday': (8, 12)
+        'Tuesday': (8, 20),
+        'Wednesday': (8, 13),
+        'Thursday': (8, 17),
+        'Friday': (8, 18),
+        'Saturday': (10, 15),
+        'Sunday': (11, 14)
     },
     'Employee2': {
         'Monday': (8, 12),
-        'Tuesday': (8, 21),
-        'Wednesday': (10, 13),
+        'Tuesday': (8, 20),
+        'Wednesday': (12, 20),
+        'Thursday': (8, 15),
+        'Friday': (9, 18),
+        'Saturday': (10, 14),
+        'Sunday': (11, 14)
+    },
+    'Employee3': {
+        'Monday': (8, 12),
+        'Tuesday': (8, 20),
+        'Wednesday': (12, 20),
+        'Thursday': (8, 15),
+        'Friday': (9, 18),
+        'Saturday': (10, 14),
+        'Sunday': (11, 14)
     }
 }
-shifts=dict()
-for i in opening.keys():
-    opening_hour=opening[i][0]
-    closing_hour=opening[i][1]
-    shifts[i]=get_possible_shifts(opening_hour=opening_hour,closing_hour=closing_hour,min_hours_per_shift=3,max_hours_per_shift=8)
 
-weekly_shifts=weekly_possible_shifts(list(shifts.values()))
-employees=availability.keys()
-days=opening.keys()
+#check_employee_availability(opening_hours=opening, availability=availability)
+
+shifts = dict()
+for i in opening.keys():
+    opening_hour = opening[i][0]
+    closing_hour = opening[i][1]
+    shifts[i] = get_possible_shifts(opening_hour=opening_hour, closing_hour=closing_hour, min_hours_per_shift=3, max_hours_per_shift=8)
+
+weekly_shifts = weekly_possible_shifts(list(shifts.values()))
+employees = availability.keys()
+days = opening.keys()
+
 model = LpProblem(name="Workforce_Optimization", sense=LpMinimize)
 
-# Decision variables: x[i][day][shift] is a binary variable representing whether employee i is assigned to shift on day
 x = {}
 for i in employees:
     for day in days:
         for shift in weekly_shifts:
             x[i, day, shift] = LpVariable(name=f"x_{i}_{day}_{shift}", cat=LpBinary)
 
+model += lpSum(x[i, day, shift] * (shift[1] - shift[0]) for i in employees for day in days for shift in weekly_shifts)
 
-
-# Objective function: minimize the total cost
-model += lpSum(x[i, day, shift] * (shift[1]-shift[0]) for i in employees for day in days for shift in weekly_shifts)
-min_weekly_hours=10
-max_weekly_hours=100
 for i in employees:
-    model += lpSum(x[i, day, shift]*(shift[1]-shift[0]) for day in days for shift in weekly_shifts) >= min_weekly_hours
-    model += lpSum(x[i, day, shift] *(shift[1]-shift[0]) for day in days for shift in weekly_shifts) <= max_weekly_hours
-
-# one shift per day for each employee 2
+    model += lpSum(x[i, day, shift] * (shift[1] - shift[0]) for day in days for shift in weekly_shifts) >= min_weekly_hours
+    model += lpSum(x[i, day, shift] * (shift[1] - shift[0]) for day in days for shift in weekly_shifts) <= max_weekly_hours
 
 for i in employees:
     for day in days:
-        model += lpSum(x[i, day, shift]  for shift in weekly_shifts) <= 1
+        model += lpSum(x[i, day, shift] for shift in weekly_shifts) <= 1
 
-
-
-
-# availability 
 for i in employees:
     for day in days:
         start, end = availability[i][day]
         for shift in weekly_shifts:
-            if shift[1] <= start or shift[0] < start or shift[0] >= end or  shift[1] > end or shift[0]< opening[day][0] or shift[1]> opening[day][1]:
+            if  not (start <= shift[0] < shift[1] <= end):
                 model += x[i, day, shift] == 0
 
-
-
-
-
-
-
-
-
-
-# Constraint: Ensure at least one employee is present at each hour during the opening hours
 for day in days:
     for hour in range(opening[day][0], opening[day][1]):
         model += lpSum(x[i, day, shift] for i in employees for shift in weekly_shifts if (hour >= shift[0] and hour < shift[1])) >= 1
 
 
-
-
-
 model.solve()
-
-
+assigned_shifts = {}
 if LpStatus[model.status] == 'Optimal':
     print("Optimal Solution Found.")
+    print(opening)
     for i in employees:
+        assigned_shifts[i] = {}
         for day in days:
             for shift in weekly_shifts:
                 if x[i, day, shift].varValue == 1:
-                    start_hour, end_hour = shift[0],shift[1]
-                    print(f" {i} is assigned to Shift on {day} from {start_hour} to {end_hour}")
+                    start_hour, end_hour = shift[0], shift[1]
+                    assigned_shifts[i][day] = (start_hour, end_hour)
+
+# Print the results
+    for i, shifts_per_day in assigned_shifts.items():
+        print(f"{i} shifts:")
+        for day, shift in shifts_per_day.items():
+            print(f"   {day}: from {shift[0]} to {shift[1]}")
+
 else:
     print("No feasible solution found.")
